5047525bf0db46e1896c2c3951f80b0effa71ef7
SamTipper <samtipperprogrammer@gmail.com>
2022-12-14 20:31:24 +0000

    Added CSS border for products table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit{
   newProductForm: FormGroup;
+  tableForm: FormGroup;
 
   ngOnInit(){
     this.newProductForm = new FormGroup({
       productName: new FormControl(null, Validators.required),
       productQuantity: new FormControl(null, [Validators.required, Validators.pattern("^[0-9]*$")])
     });
+
+    this.tableForm = new FormGroup({
+      
+    })
   }
 
   onSubmitNewProduct(){
