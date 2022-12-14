203297af2c4538f8903cb0333761573f18b2ddcc
SamTipper <samtipperprogrammer@gmail.com>
2022-12-14 19:51:15 +0000

    Added index and first form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stock-levels';
+export class AppComponent implements OnInit{
+  newProductForm: FormGroup;
+
+  ngOnInit(){
+    this.newProductForm = new FormGroup({
+      productName: new FormControl(null, Validators.required),
+      productQuantity: new FormControl(null, [Validators.required, Validators.pattern("^[0-9]*$")])
+    });
+  }
+
+  onSubmitNewProduct(){
+    console.log(this.newProductForm);
+  }
 }
